2f9b190fc9b69b88615040308a3a8a09b8c93a94
cristinapontiijas <cristinapontijas@gmail.com>
2020-03-15 12:50:02 +0100

    paint clue in clue input #1

--- js/main.js
+++ js/main.js
@@ -11,18 +11,24 @@ console.log(randomNumber);
 // get input value for player number
 const guessBtn = document.querySelector('.js-btn');
 const playerNumber = document.querySelector('.js-input-number');
+const clueInput = document.querySelector('.js-input-clue');
+const clueAttempt = document.querySelector('.js-input-attempt');
 
 function getPlayerNumber() {
-  const playerNumberValue = playerNumber.value;
+  let playerNumberValue = playerNumber.value;
   console.log(playerNumberValue);
   if (randomNumber === parseInt(playerNumberValue)) {
+    clueInput.value = '¡Olé, lo has adivinado!';
     console.log('¡Olé, lo has adivinado!');
   } else if (playerNumberValue > randomNumber) {
+    clueInput.value = 'Prueba con un número más pequeño...';
     console.log('Prueba con un número más pequeño...');
   } else if (playerNumberValue < randomNumber) {
+    clueInput.value = '¡Ui! Prueba con un número más grande...';
     console.log('¡Ui! Prueba con un número más grande...');
   } else {
-    console.log('Uhm, no sé...Prueba otra vez.');
+    clueInput.value = 'Uhm, no sé...Prueba con un número del 1 al 100.';
+    console.log('Uhm, no sé...Prueba con un número del 1 al 100.');
   }
 }
 
