9c89e3f3cd301eaeac54788e5b7a8548027602ab
cristinapontiijas <cristinapontijas@gmail.com>
2020-03-15 09:57:01 +0100

    start javascript changes, add random number generator #1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 'use strict';
 
-const guessBtn = document.querySelector('.js-input-btn');
-const guessUserNumber = document.querySelector('.js-guess-number');
-
+// random number generator
 function getRandomNumber(max) {
-  return Math.ceil(Math.random() * 100);
+  return Math.ceil(Math.random() * 101);
 }
 
-guessBtn.addEventListener('click', getRandomNumber);
+const randomNumber = getRandomNumber();
+console.log(randomNumber);
+
+// get input value for player number
+const guessBtn = document.querySelector('.js-btn');
+const playerNumber = document.querySelector('.js-input-number');
+
+function getPlayerNumber() {
+  const playerNumberValue = playerNumber.value;
+  console.log(playerNumberValue);
+  if (randomNumber === parseInt(playerNumberValue)) {
+    console.log('¡Olé, lo has adivinado!');
+  } else if (playerNumberValue > randomNumber) {
+    console.log('Prueba con un número más pequeño...');
+  } else if (playerNumberValue < randomNumber) {
+    console.log('¡Ui! Prueba con un número más grande...');
+  } else {
+    console.log('Uhm, no sé...Prueba otra vez.');
+  }
+}
+
+guessBtn.addEventListener('click', getPlayerNumber());
